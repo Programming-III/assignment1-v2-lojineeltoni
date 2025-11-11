4f40bc44f4cffa2bd41d78e40a42a129b27a1fd1
lojineeltoni <lojine.eltoni@student.giu-uni.de>
2025-11-11 10:23:43 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,7 +5,29 @@
 #include <string>
 #using namespace std;
 //#write student class here
+class Student : Person {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student() {
+        yearLevel = 0;
+        major;
 
+    }
+    Student(int yl, string m) {
+        yearLevel = yl;
+        major =m;
+    }
+    void getName():Person {
+        return p ->name;
+    }
+    ~Student (){}
+    void display() {
+        cout <<  "Year Level:" << yearLevel << "" << "Major:" << major;
+    }
+    
+};
 
 
 

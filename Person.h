e420c7c60c6e91d3562a4f4f0b86f0829cae6114
lojineeltoni <lojine.eltoni@student.giu-uni.de>
2025-11-11 10:18:34 +0300

    Update Person.h

--- Person.h
+++ Person.h
@@ -2,8 +2,28 @@
 #define PERSON_H
 
 #include <string>
+#include <iostream>
 using namespace std;
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person(string n, int i) {
+        name = n;
+        id = i;
+    }
+    Person() {
+        name;
+        id = 0;
+    }
+    
+    ~Person(){}
+    void display() {
+        cout << "name:" << name << "" << "id:" << id << endl;
+    }
 
+};
 //#write Person class here
 
 

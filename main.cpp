b1c7e32dcd2fe3a2594dfa5839cec9a161e9ad37
lojineeltoni <lojine.eltoni@student.giu-uni.de>
2025-11-11 10:25:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,28 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+#include <iostream>
+using namespace std;
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person(string n, int i) {
+        name = n;
+        id = i;
+    }
+    Person() {
+        name;
+        id = 0;
+    }
+    
+    ~Person(){}
+    void display() {
+        cout << "name:" << name << "" << "id:" << id << endl;
+    }
 
+};
 
 
 
@@ -19,13 +40,53 @@ using namespace std;
 
 // ==================== Student Class Implementation ====================
 
+class Student : Person {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student() {
+        yearLevel = 0;
+        major;
 
+    }
+    Student(int yl, string m) {
+        yearLevel = yl;
+        major =m;
+    }
+    void getName():Person {
+        return p ->name;
+    }
+    ~Student (){}
+    void display() {
+        cout <<  "Year Level:" << yearLevel << "" << "Major:" << major;
+    }
+    
+};
 
 
 
 
 // ==================== Instructor Class Implementation ====================
+class Instructor :Person {
+private:
+    string department;
+    int experienceYears;
+    Instructor() {
+        department;
+        experienceYears = 0;
 
+    }
+    Instructor(string d, int EY) {
+        department = d;
+        experienceYears = EY;
+    }
+    ~Instructor(){}
+public:
+    void display() {
+        cout << "Department:" << department << "" << "ExperienceYears:" << experienceYears << endl;
+    }
+};
 
 
 
@@ -33,7 +94,38 @@ using namespace std;
 
 // ==================== Course Class Implementation ====================
 
-
+class Course {
+private:
+    string courseCode;
+    string CourseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course() {
+        courseCode;
+        CourseName;
+        maxStudents = 0;
+        currentStudents = 0;
+        students;
+    }
+    Course(string CC, string CN, int max, int CS,Student* s) {
+        courseCode = CC;
+        CourseName = CN;
+        maxStudents = max;
+        currentStudents = CS;
+        students = s;
+    }
+    ~Course() {
+        delete[]students;
+    }
+    void addStudent(const Student& s) {
+        for (int i = 0;i < maxStudents;i++) {
+            students* a = new students;
+            
+        }
+    }
+};
 
 
 

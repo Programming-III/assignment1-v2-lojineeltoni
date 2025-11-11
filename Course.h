80e48714abfef52ac4457d7033233f93d849873e
lojineeltoni <lojine.eltoni@student.giu-uni.de>
2025-11-11 10:44:13 +0300

    Update Course.h

--- Course.h
+++ Course.h
@@ -18,7 +18,12 @@ public:
 Course(string CC, string CN, int max, int CS,Student* s) {}
 ~Course() {}
 void addStudent(const Student& s) {}
+void getCourseCode() {}
 
+void getCourseName() {}
+void getMaxStudents() {}
+void getStudents() {}
+void getCurrentStudents() {}
 
 
 
